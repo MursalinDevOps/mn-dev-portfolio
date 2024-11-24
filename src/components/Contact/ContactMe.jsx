ac9dcc0a4992037f809c0d230fbf079e61dce2e1
MursalinDevOps <mursalin1768@gmail.com>
2024-11-24 22:06:02 +0600

    footer added

--- src/components/Contact/ContactMe.jsx
+++ src/components/Contact/ContactMe.jsx
@@ -38,9 +38,11 @@ const ContactMe = () => {
                     <div className="col-span-12 md:col-span-6 flex flex-col justify-center items-start p-10 space-y-4 border-b md:border-b-0 md:border-r border-gray-500">
                         <div className="mx-auto space-y-3">
                             <p className="text-lg font-semibold text-gray-300 flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                                    <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
+                                    <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
                                 </svg>
+
 
                                 <span
                                     className="ml-3 cursor-copy relative"
@@ -57,9 +59,10 @@ const ContactMe = () => {
                                 </span>
                             </p>
                             <p className="text-lg font-semibold text-gray-300 flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                                    <path fillRule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clipRule="evenodd" />
                                 </svg>
+
 
                                 <span
                                     className="ml-3 cursor-copy relative"
--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -5,16 +5,16 @@ import Navbar from "./Navbar/Navbar";
 import TechStack from "./TechStack/TechStack";
 
 const Root = () => {
-    {/* bg-gradient-to-b from-[#003333] to-[#0a0a0a] */}
-    {/*0. bg-gradient-to-b from-[#330000] to-[#0a0a0a] */}
-    {/*1. bg-gradient-to-tr from-[#003333] to-[#000000] */}
-    {/*2. bg-gradient-to-r from-[#1a001a] to-[#0a0a0a] */}
+    {/* bg-gradient-to-b from-[#003333] to-[#0a0a0a] */ }
+    {/*0. bg-gradient-to-b from-[#330000] to-[#0a0a0a] */ }
+    {/*1. bg-gradient-to-tr from-[#003333] to-[#000000] */ }
+    {/*2. bg-gradient-to-r from-[#1a001a] to-[#0a0a0a] */ }
     return (
         <div className="w-full">
             <header className=" bg-gradient-to-b from-[#003333] to-[#0a0a0a]
              min-h-screen lg:min-h-svh">
                 {/* navbar */}
-                    <Navbar></Navbar>
+                <Navbar></Navbar>
                 {/* hero */}
                 <div className="text-base-300 flex justify-center items-center lg:mt-32">
                     <Hero></Hero>
@@ -26,12 +26,20 @@ const Root = () => {
                     <TechStack></TechStack>
                 </section>
                 <section>
-                <About></About>
+                    <About></About>
                 </section>
                 <section>
-                <ContactMe></ContactMe>
+                    <ContactMe></ContactMe>
                 </section>
             </main>
+            <footer className="bg-[#0a0a0a] py-5 text-base-300">
+                <div className="flex justify-center">
+                    Made with  <span className="px-3 text-[#923ee6]"> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                        <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+                    </svg>
+                    </span>  by Mursalin
+                </div>
+            </footer>
         </div>
     );
 };
